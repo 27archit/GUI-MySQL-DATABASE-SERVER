c2805a69e1b3df30981470685c070eb7e60cdac0
Archit <archit27nov@gmail.com>
2021-07-27 23:58:39 +0530

    main

---
+++ Database.java
@@ -0,0 +1,24 @@
+package my_db_server;
+import java.sql.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class Database implements ActionListener
+{
+Frame f;
+
+Database()
+{
+
+}
+
+public void actionPerformed(ActionEvent e)
+{
+if()
+}
+
+public static void main()
+{
+Database d=new Database();
+}
+}
